94c22b741baea34797ece0a1ea9fff546583887e
bhavesh-sarvaiya <bhaveshsarvaiya577@gmail.com>
2018-02-01 17:17:55 +0530

    first commit

---
+++ src/main/webapp/app/app.main.ts
@@ -0,0 +1,13 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { ProdConfig } from './blocks/config/prod.config';
+import { LmsAppModule } from './app.module';
+
+ProdConfig();
+
+if (module['hot']) {
+    module['hot'].accept();
+}
+
+platformBrowserDynamic().bootstrapModule(LmsAppModule)
+.then((success) => console.log(`Application started`))
+.catch((err) => console.error(err));
